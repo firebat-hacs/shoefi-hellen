ef4d2040db5eb7191cb61a44efde64bf97c6637b
Robert Plummer <rplummer@sequel.ae>
2025-08-27 09:50:32 -0400

    Remove build step that throws

--- board.mk
+++ board.mk
@@ -9,6 +9,10 @@ include $(BOARD_DIR)/meta-info.env
 # this is usually used by Bluetooth
 DDEFS += $(PRIMARY_COMMUNICATION_PORT_USART2)
 
+# Override problematic compiler flag that causes build errors
+# This removes the -Wno-delete-non-abstract-non-virtual-dtor flag from RUSEFI_OPT
+RUSEFI_OPT := $(filter-out -Wno-delete-non-abstract-non-virtual-dtor,$(RUSEFI_OPT))
+
 # this includes knock and mux control settings
 include $(BOARDS_DIR)/hellen/hellen-common-mega144.mk
 
